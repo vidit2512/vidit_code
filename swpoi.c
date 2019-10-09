59ac483e42c1429d7bc3a86e7bfe3f5e257a74e9
vidit2512 <201951167@iiitvadodara.ac.in>
2019-10-09 17:04:56 +0530

    swap using pointer

---
+++ swpoi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int *p1,int *p2)
+{
+*p1=*p1+*p2;
+*p2=*p1-*p2;
+*p1=*p1-*p2;
+}
+void main()
+{ int w1,w2;
+printf("enter the 2 numbers ");
+scanf("%d",&w1);
+scanf("%d",&w2);
+swap(&w1,&w2);
+printf("the num are %d  %d",w1,w2);
+}
