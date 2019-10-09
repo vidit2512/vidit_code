621bb8c2f849837b98bf0dbbd42d96cce860324a
Chirag-Mathur <chiragmathurindia@gmail.com>
2019-10-09 19:34:33 +0530

    entering name

--- add.c
+++ add.c
@@ -6,10 +6,15 @@ int a;
 int b;
 int c;
 
+char *name;
+
+scanf("%s",&name);
+printf("%s\n",name);
+
 printf("Enter the three numbers : ");
 scanf("%d %d %d", &a, &b, &c);
 
 printf("Sum : " , (a+b+c));
 
-retun 0;
+return 0;
 }
